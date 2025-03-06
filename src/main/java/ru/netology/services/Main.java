1e313f86e1a87ab167141fa448b7968825caad4a
Asja <anastasjamedved@gmail.com>
2025-03-06 19:40:28 +0300

    corrected commit

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -4,12 +4,19 @@ package ru.netology.services;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-RestService service = new RestService();
+        RestService service = new RestService();
         int income = 10_000;
         int expenses = 3_000;
         int threshold = 20_000;
 
         int monthRest = service.calculate(income, expenses, threshold);
         System.out.println("фрилансер отдыхает " + monthRest + " месяца в году");
-        }
+
+        income = 100_000;
+        expenses = 60_000;
+        threshold = 150_000;
+        monthRest = service.calculate(income, expenses, threshold);
+
+        System.out.println("фрилансер отдыхает " + monthRest + " месяца в году");
+    }
     }
--- src/test/java/RestServiceTest.java
+++ src/test/java/RestServiceTest.java
@@ -5,7 +5,7 @@ import ru.netology.services.RestService;
 public class RestServiceTest {
 
     @Test
-    void sumMonthRestInYear () {
+    void sumMonthRestInOneYear () {
         RestService service = new RestService();
         int income = 10_000;
         int expenses = 3_000;
@@ -14,5 +14,17 @@ public class RestServiceTest {
 
         int actual = service.calculate(income, expenses, threshold);
 Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void sumMonthRestInTwoYear () {
+        RestService service = new RestService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
     }
 }
